e84270163dc408e5f2c14709538008c550268f72
jkoers29 <jkoers29@gmail.com>
2014-04-03 19:48:57 -0700

    Initial commit for local git

---
+++ XcodeTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XcodeTest
+//
+//  Created by John R. Koerschgen on 4/3/14.
+//  Copyright (c) 2014 Me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
